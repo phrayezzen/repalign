08e2f30996833da8a62d6591cd778df68d42977d
Xilin Liu <me@xilinliu.com>
2025-09-18 02:05:12 -0400

    Initial Commit

---
+++ RepAlignTests/RepAlignTests.swift
@@ -0,0 +1,17 @@
+//
+//  RepAlignTests.swift
+//  RepAlignTests
+//
+//  Created by Xilin Liu on 9/18/25.
+//
+
+import Testing
+@testable import RepAlign
+
+struct RepAlignTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
